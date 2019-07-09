647390bd90823ad3ecfb6c1bc1249b089cd2c66e
rise289 <rise289@gmail.com>
2019-07-09 15:09:47 -0500

    Design Patterns Base

---
+++ src/com/designpatterns/base/Touring.java
@@ -0,0 +1,13 @@
+package com.designpatterns.base;
+
+import com.designpatterns.java.BikeColor;
+
+public class Touring extends RoadBike {
+    public Touring(WheelInterface wheel, BikeColor color){
+        super(wheel,color);
+    }
+
+    public Touring(WheelInterface wheel){
+        this(wheel, BikeColor.BLUE);
+    }
+}
---
+++ src/com/designpatterns/java/Main.java
@@ -0,0 +1,10 @@
+package com.designpatterns.java;
+
+public class Main {
+/*
+*
+* */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
